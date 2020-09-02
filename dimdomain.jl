3b2cea0fe2869aaaf873f3aeae3f30d9a8d3868b
Rafael Schouten <rafaelschouten@gmail.com>
2020-09-03 00:43:35 +1000

    use DimColumns

--- dimdomain.jl
+++ dimdomain.jl
@@ -1,14 +1,41 @@
+# ] add DimensionalData#tables
+
 using DimensionalData, GeoStatsBase
 
-struct DimDomain{D} <: AbstractDomain
-    dims::D
+
+struct DimDomain{C} <: AbstractDomain
+    dimcolumns::C
 end
 DimDomain(dims::Tuple{Vararg{<:Dimension}}) = begin
-    # Do some type conversions / checks here
-    DimDomain{typeof(dims)}(dims)
+    dimcolumns = map(d -> DimColumn(d, dims), dims)
+    DimDomain(dimcolumns)
 end
 
-GeoStatsBase.nelms(dom::DimDomain) = product(map(length, dims(dom)))
+columns(dom::DimDomain) = dom.columns
+
+GeoStatsBase.nelms(dom::DimDomain) = prod(map(length, dims(dom)))
 GeoStatsBase.ncoords(dom::DimDomain{Tuple{Vararg{<Dimension,N}}}) where N = N
 GeoStatsBase.coordtype(dom::DimDomain) = eltype(first(dims(dom)))
-GeoStatsBase.coordinates!(buf, dom::DimDomain, ind) = nothing # compute the coords from dims
+GeoStatsBase.coordinates!(buf, dom::DimDomain, i::Int) = map(c -> c[i], columns(dom))
+
+
+#= Discussion point: the domain is nearly identical to the table: 
+
+struct DimTable{Keys,A,C} <: Tables.AbstractColumns 
+    array::A
+    dimcolumns::C
+end
+
+This begs the question: why is the domain even separate?
+The key thing that needs to be specified is which columns are domain,
+which are values. 
+
+That could be done with extra interface methods for AbstractTable
+
+- `dimcolnames`/`domaincolnames`
+- `valcolnames`
+
+Or something similar. 
+
+Then you can implement coordinates! generically 
+on any table that defines `dimcolnames` or whatever it is.
