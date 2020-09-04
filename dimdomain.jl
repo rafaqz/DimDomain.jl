5eb4546cbadbbac353f4a5e9d1195b8060eaca7b
Rafael Schouten <rafaelschouten@gmail.com>
2020-09-05 03:54:49 +1000

    working test case

--- dimdomain.jl
+++ dimdomain.jl
@@ -1,6 +1,6 @@
 
 using DimensionalData, GeoStatsBase, GeoStats, Plots, 
-      DirectGaussianSimulation, Test
+      DirectGaussianSimulation, Test, StaticArrays, LinearAlgebra
 
 using DimensionalData: DimColumn
 
@@ -31,40 +31,45 @@ dimcolumns(dom::DimDomain) = dom.dimcolumns
 # This syntax should be fixed in DD for DimColumn
 DimensionalData.dims(dom::DimDomain) = map(DimensionalData.dim, dimcolumns(dom))  
 
+
+# GeoStatsBase interface
 GeoStatsBase.nelms(dom::DimDomain) = length(first(dimcolumns(dom)))
-
 GeoStatsBase.ncoords(dom::DimDomain) = length(dimcolumns(dom))
-
 GeoStatsBase.coordtype(dom::DimDomain) = eltype(first(dimcolumns(dom)))
-
-GeoStatsBase.coordinates!(buf, dom::DimDomain, i::Int) = 
-    map(c -> c[i], dimcolumns(dom))
+GeoStatsBase.coordinates!(buf::AbstractVector, dom::DimDomain, i::Int) = 
+    buf[i] = SA[map(c -> c[i], dimcolumns(dom))...]
+GeoStatsBase.coordinates!(buf::MArray, dom::DimDomain, i::Int) =
+    buf .= map(c -> c[i], dimcolumns(dom))
 
 
 # Define a regular index DimArray
-da = DimArray(rand(20, 30), (X(-19.0:2.0:19.0), X(1.0:3.0:90.0)))
-D = DimDomain(da)
+da = DimArray(rand(20, 30), (X(-19.0:2.0:19.0), Y(3.0:3.0:90.0)))
+rg = DimDomain(da)
 # Get a RegularGrid domain
-@test D isa RegularGrid
+@test rg isa RegularGrid
 
-P = SimulationProblem(D, :Z => Float64, 2)
+P = SimulationProblem(rg, :Z => Float64, 2)
 S  = DirectGaussSim(:Z=>(variogram=GaussianVariogram(range=30.0),))
 sol = solve(P, S)
 plot(sol)
 
-# Define an Irregular index DimArray
-da = DimArray(rand(9, 20), (X([1, 1, 2, 3, 5, 8, 13, 21, 34]), X(1.0:3.0:60.0)))
+# Define an identical but "Irregular" marked DimArray 
+da = DimArray(zeros(20, 30), (X([-19.0:2.0:19.0...]), Y([3.0:3.0:90.0...])))
 D = DimDomain(da)
 # Get a DimDomain
 @test D isa DimDomain
+@test GeoStatsBase.nelms(D) == 20 * 30
+@test GeoStatsBase.ncoords(D) == 2
+@test GeoStatsBase.coordtype(D) == Float64
 
+# It works jus the same
 P = SimulationProblem(D, :Z => Float64, 2)
 S  = DirectGaussSim(:Z=>(variogram=GaussianVariogram(range=30.0),))
-# This errors 1 in 10 times with:
-# ERROR: PosDefException: matrix is not positive definite; Cholesky factorization failed.
 sol = solve(P, S)
-# How to make plotting work? Could the interface make this easier?
+# But doesn't plot. This could just work using the interface?
 # plot(sol)
+
+
 
 
 #= Discussion point: the domain is nearly identical to the table: 
